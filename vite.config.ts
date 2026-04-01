28d5a1a97f948e93bb2934070beb3e7c9463c507
DankoKr <dankokralski776@gmail.com>
2026-04-01 09:04:21 +0300

    possible fix for the 404 on deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/mind-hijack-website/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
