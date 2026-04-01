8922f5a83521b6af1ce38f7280e4401922e9bec9
DankoKr <dankokralski776@gmail.com>
2026-04-01 09:11:32 +0300

    possible fix for the url

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -15,12 +15,12 @@ const App = () => (
       <LanguageProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter basename="/mind-hijack-website">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
       </LanguageProvider>
     </TooltipProvider>
   </QueryClientProvider>
